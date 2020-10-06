fe8079e9841197cb772f88210d7ea4a8474e1ba6
fahdiazhannu <zanuazhannu@gmail.com>
2020-10-06 13:59:20 +0700

    Membuat konfirmasi.php

--- pertemuan-4/konfirmasi.php
+++ pertemuan-4/konfirmasi.php
@@ -6,9 +6,9 @@ if (isset($_POST['registerbtn'])){
     $namaDepan = $_POST['namaDepan'];
     $namaBelakang = $_POST['namaBelakang'];
 
-    echo $username;
-    echo "<br>" .$email;
-    echo "<br> ".$namaDepan." ".$namaBelakang;
+    echo "Username : ".$username;
+    echo "<br> Email : " .$email;
+    echo "<br> Nama Lengkap ".$namaDepan." ".$namaBelakang;
 }
 
 
