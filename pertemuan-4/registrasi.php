4d136fae84bf574a2054cb87d52fd2d7c13085a8
fahdiazhannu <zanuazhannu@gmail.com>
2020-10-09 19:06:41 +0700

    halaman registrasi

--- pertemuan-4/registrasi.php
+++ pertemuan-4/registrasi.php
@@ -1,96 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  background-color: black;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-/* Add padding to containers */
-.container {
-  padding: 16px;
-  background-color: white;
-}
-
-/* Full-width input fields */
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-
-input[type=text]:focus, input[type=password]:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Overwrite default styles of hr */
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-}
-
-/* Set a style for the submit button */
-.registerbtn {
-  background-color: #4CAF50;
-  color: black;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-.registerbtn:hover {
-  opacity: 1;
-}
-
-/* Add a blue text color to links */
-a {
-  color: dodgerblue;
-}
-
-/* Set a grey background color and center the text of the "sign in" section */
-.signin {
-  background-color: #f1f1f1;
-  text-align: center;
-}
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
 </head>
+    <style>
+    .inputan{
+        width: 500px;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+    }
+    input[type=submit]{
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 16px 32px;
+        text-decoration:none;
+        margin:4px 2px;
+        cursor: pointer;
+    }
+    </style>
 <body>
-
-<form action="konfirmasi.php" method="post">
-  <div class="container">
-
-    <label for="username"><b>Username</b></label>
-    <input type="text" placeholder="Username" name="username" required>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder=" Email" name="email" id="email" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder=" Password" name="psw" id="psw" required>
-
-    <label for="namadepan"><b>Nama Depan</b></label>
-    <input type="text" placeholder="Nama Depan" name="namaDepan" required>
-
-    <label for="namabelakang"><b>Nama Belakang</b></label>
-    <input type="text" placeholder="Nama Belakang" name="namaBelakang" required>
-
-    <button type="submit" class="registerbtn" name="registerbtn">Register</button>
-  </div>
-  
-</form>
-
+    <p>Login System</p>
+    <form method="post" action="ceklogin.php">
+        <label >nama depan</label> <br>
+        <input class="inputan" type="text" name="namadpn"> <br>
+        <label >nama belakang</label> <br>
+        <input class="inputan" type="text" name="namablk" > <br>
+        <label >Email</label> <br>
+        <input class="inputan" type="text" name="email" > <br>
+        <label >Username</label> <br>
+        <input class="inputan" type="text" name="username"> <br>
+        <label >password</label> <br>
+        <input class="inputan" type="password" name="password" > <br>
+        <input type="submit" name="tombolsubmit" value="Daftar">
+    </form>    
 </body>
 </html>
